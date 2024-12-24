0ca3cba4bd5761697057186dfe1cc0f330f5394e
daks <daks4012@naver.com>
2024-12-24 19:23:22 +0900

    Add Link and Nav to App Files

--- section02/src/pages/book/[[...id]].tsx
+++ section02/src/pages/book/[[...id]].tsx
@@ -3,6 +3,5 @@ import { useRouter } from "next/router"
 export default function Page() {
       const router = useRouter();
       const q = router.query.id;
-      console.log(q);
       return <h1>{q}번째 Book page</h1>
 }
