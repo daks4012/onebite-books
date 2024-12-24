c031eb7ef5791fa430263a19c69fca0c906aaca2
daks <daks4012@naver.com>
2024-12-24 18:47:15 +0900

    라우팅 설정 챕터 완료

--- section02/src/pages/book/[[...id]].tsx
+++ section02/src/pages/book/[[...id]].tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router"
 
 export default function Page() {
       const router = useRouter();
-      const {q} = router.query;
-      return <h1>서치 페이지{q}</h1>
+      const q = router.query.id;
+      console.log(q);
+      return <h1>{q}번째 Book page</h1>
 }
