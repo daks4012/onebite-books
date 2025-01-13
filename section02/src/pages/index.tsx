440e21ed567bd64af3297e463665bb3d73a9357a
daks <daks4012@naver.com>
2025-01-13 18:25:59 +0900

    Styling-css module

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,3 +1,8 @@
+import style from "../styles/index.module.css"
+
 export default function Home() {
-  return <h1>인덱스 페이지</h1>
+  return <>
+  <h1 className={style.h1}>인덱스 페이지</h1>
+  <h2 className={style.h2}>이건 블루로</h2>
+  </>
 }
