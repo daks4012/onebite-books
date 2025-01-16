ed21b84cbfbe618b6cae71aa63e78c4324385797
daks <daks4012@naver.com>
2025-01-16 20:06:00 +0900

    global-layout styling

---
+++ section02/src/components/global-layout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import Link from "next/link"
+import style from "./global-layout.module.css"
+
+export default function GlobalLayout({
+      children,
+}: {
+      children: ReactNode;
+
+}) {
+return(<div className={style.container}>
+      <header className={style.header}>
+      <Link href='/'>📚 OneBite Books</Link>ㅁㄴㅇ
+      </header>
+      <main className={style.main}>{children}</main>
+      <footer className={style.footer}>제작 @Daks</footer>
+</div>);
+}
